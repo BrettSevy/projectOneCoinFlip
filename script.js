938ff3e940e12d4e315c75cc514602f7da37bfd8
Brett Sevy <justbrettt@gmail.com>
2020-02-04 10:28:45 -0600

    added api keys

---
+++ script.js
@@ -0,0 +1,32 @@
+var settings = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://the-cocktail-db.p.rapidapi.com/list.php?a=list",
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "the-cocktail-db.p.rapidapi.com",
+		"x-rapidapi-key": "fc5f9a45b1mshee0ed2db4af132cp10d4e9jsn16a42c447127"
+	}
+}
+
+$.ajax(settings).done(function (response) {
+	console.log(response);
+});
+
+
+var settings = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://world-clock.p.rapidapi.com/json/utc/now",
+	"method": "",
+	"headers": {
+		"x-rapidapi-host": "world-clock.p.rapidapi.com",
+		"x-rapidapi-key": "fc5f9a45b1mshee0ed2db4af132cp10d4e9jsn16a42c447127",
+		"content-type": "application/x-www-form-urlencoded"
+	},
+	"data": {}
+}
+
+$.ajax(settings).done(function (response) {
+	console.log(response);
+});
