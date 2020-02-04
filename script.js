9b82e8866d24b4cf44ccd5e40b0a4393b4b9dff0
Brett Sevy <justbrettt@gmail.com>
2020-02-04 10:53:13 -0600

    added the rest of the api's

--- script.js
+++ script.js
@@ -1,7 +1,53 @@
 var settings = {
 	"async": true,
 	"crossDomain": true,
-	"url": "https://the-cocktail-db.p.rapidapi.com/list.php?a=list",
+    "url": "https://the-cocktail-db.p.rapidapi.com/list.php?a=list",
+    
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "the-cocktail-db.p.rapidapi.com",
+		"x-rapidapi-key": "fc5f9a45b1mshee0ed2db4af132cp10d4e9jsn16a42c447127"
+	}
+}
+
+$.ajax(settings).done(function (response) {
+	console.log(response);
+});
+
+var settings = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://the-cocktail-db.p.rapidapi.com/list.php?c=list",
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "the-cocktail-db.p.rapidapi.com",
+		"x-rapidapi-key": "fc5f9a45b1mshee0ed2db4af132cp10d4e9jsn16a42c447127"
+	}
+}
+
+$.ajax(settings).done(function (response) {
+	console.log(response);
+});
+
+var settings = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://the-cocktail-db.p.rapidapi.com/list.php?i=list",
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "the-cocktail-db.p.rapidapi.com",
+		"x-rapidapi-key": "fc5f9a45b1mshee0ed2db4af132cp10d4e9jsn16a42c447127"
+	}
+}
+
+$.ajax(settings).done(function (response) {
+	console.log(response);
+});
+
+var settings = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://the-cocktail-db.p.rapidapi.com/list.php?g=list",
 	"method": "GET",
 	"headers": {
 		"x-rapidapi-host": "the-cocktail-db.p.rapidapi.com",
